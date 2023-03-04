a74c3927649c6c20618c653b150928a920b47d19
AswinAsok <aswinasokofficial@gmail.com>
2023-03-04 15:24:20 +0530

    dev:basic-ui-setting

--- src/Pages/FileUpload/FileUpload.jsx
+++ src/Pages/FileUpload/FileUpload.jsx
@@ -7,6 +7,7 @@ const FileUpload = () => {
     <div className={styles.main_container}>
       <div className={styles.first_view_container}>
         <div className={styles.fv_texts}>
+          <img src="/fvimg.png" alt="" className={styles.fv_image} />
           <p className={styles.fv_heading}>MailMan Mass Mailer</p>
           <p className={styles.fv_tagline}>
             Your go-to open source mail service for effortlessly sending group
@@ -14,44 +15,44 @@ const FileUpload = () => {
           </p>
         </div>
         <div className={styles.first_view}>
+          <div className={styles.row}>
+            <label className={styles.form_label} htmlFor="frommail">
+              From Mail
+            </label>
+            <input className={styles.form_field} type="email" name="frommail" />
+          </div>
 
-        <div className={styles.row}>
-          <label className={styles.form_label} htmlFor="frommail">
-            From Mail
-          </label>
-          <input className={styles.form_field} type="email" name="frommail" />
+          <div className={styles.row}>
+            <label className={styles.form_label} htmlFor="appid">
+              App ID
+            </label>
+            <input className={styles.form_field} type="text" name="appid" />
+          </div>
+
+          <div className={styles.row}>
+            <label className={styles.form_label} htmlFor="textarea">
+              Email Content
+            </label>
+            <textarea name="textarea" id=""></textarea>
+          </div>
+
+          <div className={styles.row}>
+            <label className={styles.form_label} htmlFor="file">
+              Choose a file
+            </label>
+            <input
+              className={styles.form_field}
+              type="file"
+              name="file"
+              id="file"
+              accept=".csv"
+            />
+          </div>
+
+          <button className={styles.submit_button}>Submit</button>
         </div>
-
-        <div className={styles.row}>
-          <label className={styles.form_label} htmlFor="appid">
-            App ID
-          </label>
-          <input className={styles.form_field} type="text" name="appid" />
-        </div>
-
-        <div className={styles.row}>
-          <label className={styles.form_label} htmlFor="textarea">
-            Email Content
-          </label>
-          <textarea name="textarea" id=""></textarea>
-        </div>
-
-        <div className={styles.row}>
-          <label className={styles.form_label} htmlFor="file">
-            Choose a file
-          </label>
-          <input
-            className={styles.form_field}
-            type="file"
-            name="file"
-            id="file"
-            accept=".csv"
-          />
-        </div>
-
-        <button className={styles.submit_button}>Submit</button>
       </div>
-    </div></div>
+    </div>
   );
 };
 
